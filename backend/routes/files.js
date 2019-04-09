05f00959427fac402162a1ececcbbd6e092fbfc0
129Camal <frederico21pinto@hotmail.com>
2019-04-10 00:10:10 +0100

    Already creates CSV from mzML!

--- backend/routes/files.js
+++ backend/routes/files.js
@@ -3,12 +3,70 @@ var router = express.Router();
 var jsmzml = require('js_mzml');
 var fs = require('fs')
 const multer = require('multer')
+var Regex = require("regex");
 const exec = require('child_process').exec;
+const createCsvWriter = require('csv-writer').createArrayCsvWriter;
 
+function getContent(filename, callback){
+    
+  file = 'RAW/' + filename + '.mzML' ;
+
+  let mzml = new jsmzml(file);
+  
+  let options = {
+    'level': '1',
+    'rtBegin': 0,
+    'rtEnd': 9999999999
+  };
+
+  let spectra = mzml.retrieve(options, () => {
+    let matrix = []
+    let sumIntensity = new Array(Object.keys(mzml.spectra).length).fill(0);
+    let numScans = Array.apply(null, {length: Object.keys(mzml.spectra).length}).map(Number.call, Number)
+    
+    let header = Array.apply(null, {length: 1000}).map(Number.call, Number)
+    
+    
+    matrix[0] = Array.apply(null, {length: 1000}).map(Number.call, Number)
+    for(let i = 0; i < Object.keys(mzml.spectra).length; i++){
+      matrix[i+1] = new Array(1000).fill(0);
+      let key = Object.keys(mzml.spectra)[i]
+  
+      for(let j = 0; j< mzml.spectra[key].mass.length; j++){
+        let mass = Math.round(mzml.spectra[key].mass[j])
+        let intensity = mzml.spectra[key].intensity[j]
+        
+        if(intensity){
+          matrix[i+1][mass-1] = Number.parseFloat(intensity.toFixed(2))
+        } 
+      }
+      sumIntensity[i] = parseFloat(matrix[i+1].reduce((acc, actual) => acc + actual).toFixed(2))
+    }
+
+    let csvWriter = createCsvWriter({
+      header: header,
+      path: './csv/' + filename + '.csv'
+    })
+    
+    csvWriter.writeRecords(matrix)       // returns a promise
+      .then(() => {
+        console.log('File Saved!');
+      });
+
+    var output = []
+
+    var result = {
+      scans: numScans,
+      intensity: sumIntensity
+    }
+    output.push(result)
+    callback(output)
+  });
+}
 
 //Set storage engine
 const storage = multer.diskStorage({
-  destination: './mzML/',
+  destination: './RAW/',
   filename: (req, file, cb) =>{
     cb(null, file.originalname)
   }
@@ -23,72 +81,40 @@ router.get('/', (req, res) =>{
 })
 
 router.post('/import', (req, res) => {
-  upload(req, res, (errupl) =>{
+   upload(req, res, (errupl) =>{
     if(!errupl){
-      let testscript = exec('docker run --rm -e WINEDEBUG=-all -v /Users/fredericopinto/Documents/LEI-19/backend/mzML:/mzML/ rawconverter wine msconvert ')// + req.files[0].originalname);
-    
-      testscript.stderr.on('data', function(data){
-        console.log("ERRO NA CONVERSÃO DO FICHEIRO");
-      });
+      let fileRaw = req.files[0].originalname.replace(/(\s)+/g, '\\ ');
+      let fileMzml = fileRaw.split('.')[0] + '.mzML'
+      
+      if(fileRaw.split('.')[1] != "RAW"){ 
+        exec('rm ' + __dirname + '/../RAW/' + file);
+        res.send({error: "FILE TYPE ERROR"}) 
+      }
+      
+      else { 
+        let error = 0
+        let testscript = exec('docker run --rm -e WINEDEBUG=-all -v '+ __dirname +'/../RAW:/data/ rawconverter wine msconvert ' + fileRaw);
+        
+        testscript.stderr.on('data', (data) => {
+          error = 1
+          exec('rm ' + __dirname + '/../RAW/' + fileRaw);
+          exec('rm ' + __dirname + '/../RAW/' + fileMzml);
+          res.send({error: "ERROR CONVERTING THE FILE"})
+        });
 
-      //testscript.on('close', (data) => {
-      //  console.log("Conversão efetuada com sucesso!");
-      //});
+        testscript.on('exit', () => {
+          if(error == 0){
+            getContent(fileRaw.split('.')[0], (output) => {
+              exec('rm ' + __dirname + '/../RAW/' + fileMzml);
+              res.jsonp(output)
+            })
+          }
+        })
+      }
     } else {
       console.log(errupl)
     }
   })
-  console.log(req.body)
-  res.redirect('/file')
-})
-
-/* GET home page. */
-router.get('/mzml', (req, res, next) => {
-  
-  filename = 'mzML/mydata.mzML';
-
-  let mzml = new jsmzml(filename);
-  
-  let options = {
-    'level': '1',
-    'rtBegin': 0,
-    'rtEnd': 9999999999
-  };
-
-  let spectra = mzml.retrieve(options, () => {
-    let matrix = []
-    let sumIntensity = new Array(Object.keys(mzml.spectra).length).fill(0);
-    let numScans = Array.apply(null, {length: Object.keys(mzml.spectra).length}).map(Number.call, Number)
-    
-    matrix[0] = Array.apply(null, {length: 1000}).map(Number.call, Number)
-    
-    for(let i = 0; i < Object.keys(mzml.spectra).length; i++){
-      matrix[i+1] = new Array(1000).fill(0);
-      let key = Object.keys(mzml.spectra)[i]
-
-      for(let j = 0; j< mzml.spectra[key].mass.length; j++){
-        let mass = Math.round(mzml.spectra[key].mass[j])
-        let intensity = mzml.spectra[key].intensity[j]
-        
-        if(intensity){
-          matrix[i+1][mass-1] = Number.parseFloat(intensity.toFixed(2))
-        } 
-      }
-      sumIntensity[i] = parseFloat(matrix[i+1].reduce((acc, actual) => acc + actual).toFixed(2))
-      
-    }
-    
-    var output = []
-
-    var result = {
-      scans: numScans,
-      intensity: sumIntensity
-    }
-
-    output.push(result)
-    res.jsonp(output)
-    
-  });
 })
 
 router.get('/getfile', (req, res, next) => {
